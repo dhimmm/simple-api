efa5f01235008e915ae9a17efde5d381a1c373f9
dima.dovgopol <dmytro.d@jetbase.io>
2024-11-03 20:16:23 +0200

    Added project files

---
+++ index.js
@@ -0,0 +1,21 @@
+const fastify = require("fastify")({ logger: true });
+
+fastify.get("/", async () => {
+  return { message: "Hello world!" };
+});
+
+fastify.get("/health", async () => {
+  return { status: "ok" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    fastify.log.info(`Server running on http://localhost:3000`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
